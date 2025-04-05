6bc1d0b9c15f724e4b6b2b30801242d5d35f1e50
Joy Offere <113602216+JoyOffere@users.noreply.github.com>
2025-04-05 13:03:53 +0200

    Update configuration for preforemance and security

--- event-locator-backend/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = require('./server'); // Assuming server.js exports the app
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
